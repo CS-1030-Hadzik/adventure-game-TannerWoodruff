eb7062d5b5d00d013dddc5f523223cc78288d736
Tanner Woodruff <tannerwoodruff1@mail.weber.edu>
2025-01-16 10:03:26 -0700

    ADD: Doc string for program info, class notes, etc.

--- game.py
+++ game.py
@@ -1,2 +1,24 @@
+'''
+Apparently the official name for this junk is "Doc String". Typically put at the beginning of code.
+Also known as a multi-line comment, used to conve information from coder to reader.
+
+"Adventure Game"
+Author: Tanner Woodruff
+Version: 1.0
+Description:
+This is a text-based adventure game where the player makes choices
+to navigate through a mysterious forest.
+'''
+
+# Welcome message and introduction.
 print("Welcome to the Adventure Game!")
 print("Your journey begins here...")
+
+#LET'S GOOOOOO IT'S PYTHON TIME
+## Maybe I should consider speaking with the data science sector of the college? See if that's interesting?
+###Shut up- focus on the class.
+
+# "CTRL + /" IS A COMMENT SHORTCUT
+
+# Asking the user/player for their name.
+print("Tell us your name, traveller.")
